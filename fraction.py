23e7a37100a89bebe7042be8d955ca34373ea527
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2021-10-27 13:24:23 +0530

    added addition and subtraction for fractions

---
+++ fraction.py
@@ -0,0 +1,56 @@
+from dataclasses import dataclass
+
+
+@dataclass(frozen=True)
+class Fraction:
+    _numerator: int      # Numerator
+    _denomenator: int    # Denomenator
+
+    def gcd(self, a, b):
+        if a > b:
+            small = b
+        else:
+            small = a
+        for i in range(1, small+1):
+            if ((a%i) == 0 and (b%i) == 0):
+                gcd = i
+
+        return gcd
+
+    def __add__(self, f):
+        if self._denomenator == f._denomenator:
+            n = self._numerator + f._numerator
+            d = self._denomenator
+        else:
+            lcm = int(abs(self._denomenator * f._denomenator) / self.gcd(self._denomenator, f._denomenator))
+            ns = int(lcm / self._denomenator) * self._numerator
+            nf = int(lcm / f._denomenator) * f._numerator
+            n = ns + nf
+            d = lcm
+
+        return Fraction(n, d)
+
+
+    def __sub__(self, f):
+        if self._denomenator == f._denomenator:
+            n = self._numerator - f._numerator
+            d = self._denomenator
+        else:
+            lcm = int(abs(self._denomenator * f._denomenator) / self.gcd(self._denomenator, f._denomenator))
+            ns = int(lcm / self._denomenator) * self._numerator
+            nf = int(lcm / f._denomenator) * f._numerator
+            n = ns - nf
+            d = lcm
+
+        return Fraction(n, d)
+
+
+def main():
+    a = Fraction(1, 2)
+    b = Fraction(3, 4)
+
+    print(a, b)
+
+
+if __name__ == "__main__":
+    main()
