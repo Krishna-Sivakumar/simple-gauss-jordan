68813c503320428f09b917650232520209b739ad
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2021-10-28 12:56:20 +0530

    transitioned to numpy

--- fraction.py
+++ fraction.py
@@ -1,28 +1,27 @@
 from dataclasses import dataclass
+import numpy as np
 
 
-@dataclass(frozen=True)
+@dataclass(unsafe_hash=True)
 class Fraction:
     _numerator: int      # Numerator
     _denomenator: int    # Denomenator
 
-    def gcd(self, a, b):
-        if a > b:
-            small = b
-        else:
-            small = a
-        for i in range(1, small+1):
-            if ((a%i) == 0 and (b%i) == 0):
-                gcd = i
+    def __init__(self, numerator, denomenator):
+        self._numerator = numerator
+        self._denomenator = denomenator
 
-        return gcd
+        gcd = np.gcd(numerator, denomenator)
+        if gcd > 1:
+            self._numerator = int(numerator / gcd)
+            self._denomenator = int(denomenator / gcd)
 
     def __add__(self, f):
         if self._denomenator == f._denomenator:
             n = self._numerator + f._numerator
             d = self._denomenator
         else:
-            lcm = int(abs(self._denomenator * f._denomenator) / self.gcd(self._denomenator, f._denomenator))
+            lcm = int(abs(self._denomenator * f._denomenator) / np.gcd(self._denomenator, f._denomenator))
             ns = int(lcm / self._denomenator) * self._numerator
             nf = int(lcm / f._denomenator) * f._numerator
             n = ns + nf
