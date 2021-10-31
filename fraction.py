cdde0d655883639bf045ad46f722f46995766697
Krishna Sivakumar <krishnasivaprogrammer@gmail.com>
2021-11-01 03:04:20 +0530

    renamed denomenator to denominator

--- fraction.py
+++ fraction.py
@@ -5,39 +5,38 @@ import numpy as np
 @dataclass(unsafe_hash=True)
 class Fraction:
     _numerator: int      # Numerator
-    _denomenator: int    # Denomenator
+    _denominator: int    # Denomenator
 
-    def __init__(self, numerator, denomenator):
+    def __init__(self, numerator, denominator):
         self._numerator = numerator
-        self._denomenator = denomenator
+        self._denominator = denominator
 
-        gcd = np.gcd(numerator, denomenator)
+        gcd = np.gcd(numerator, denominator)
         if gcd > 1:
             self._numerator = int(numerator / gcd)
-            self._denomenator = int(denomenator / gcd)
+            self._denominator = int(denominator / gcd)
 
     def __add__(self, f):
-        if self._denomenator == f._denomenator:
+        if self._denominator == f._denominator:
             n = self._numerator + f._numerator
-            d = self._denomenator
+            d = self._denominator
         else:
-            lcm = int(abs(self._denomenator * f._denomenator) /
-                      np.gcd(self._denomenator, f._denomenator))
-            ns = int(lcm / self._denomenator) * self._numerator
-            nf = int(lcm / f._denomenator) * f._numerator
+            lcm = int(abs(self._denominator * f._denominator) / np.gcd(self._denominator, f._denominator))
+            ns = int(lcm / self._denominator) * self._numerator
+            nf = int(lcm / f._denominator) * f._numerator
             n = ns + nf
             d = lcm
 
         return Fraction(n, d)
 
     def __sub__(self, f):
-        if self._denomenator == f._denomenator:
+        if self._denominator == f._denominator:
             n = self._numerator - f._numerator
-            d = self._denomenator
+            d = self._denominator
         else:
-            lcm = int(abs(self._denomenator * f._denomenator) / np.gcd(self._denomenator, f._denomenator))
-            ns = int(lcm / self._denomenator) * self._numerator
-            nf = int(lcm / f._denomenator) * f._numerator
+            lcm = int(abs(self._denominator * f._denominator) / np.gcd(self._denominator, f._denominator))
+            ns = int(lcm / self._denominator) * self._numerator
+            nf = int(lcm / f._denominator) * f._numerator
             n = ns - nf
             d = lcm
 
@@ -45,13 +44,13 @@ class Fraction:
 
     def __mul__(self, f):
         n = self._numerator * f._numerator
-        d = self._denomenator * f._denomenator
+        d = self._denominator * f._denominator
 
         return Fraction(n, d)
 
     def __truediv__(self, f):
-        n = self._numerator * f._denomenator
-        d = self._denomenator * f._numerator
+        n = self._numerator * f._denominator
+        d = self._denominator * f._numerator
 
         if n == 0:
             return Fraction(0, 1)
